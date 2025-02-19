eec24ab3f120e538cbfa5836dbc56c6d11ede582
Astorcam <62352203+astorcam@users.noreply.github.com>
2025-02-19 21:27:46 +0100

    Updates

--- js/photo-gallery.js
+++ js/photo-gallery.js
@@ -1,11 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("DOM loaded");
-    let elements = document.querySelectorAll('.row:has(img[alt="photo-gallery"])');
+
+    const images = document.querySelectorAll('img[alt="photo-gallery"]');
+    const elements = document.querySelectorAll('.row:has(img[alt="photo-gallery"])');
+    let loadedImages = 0;
+
+    function checkImagesLoaded() {
+        loadedImages++;
+        if (loadedImages === images.length) {
+            document.getElementById("preloader").style.display = "none";
+            document.getElementById("content").style.display = "flex";
+        }
+    }
+
+    images.forEach(img => {
+        if (img.complete) {
+            checkImagesLoaded();
+        } else {
+            img.onload = checkImagesLoaded;
+        }
+    });
 
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add("visible");
+                observer.unobserve(entry.target); // Dejar de observar una vez visible
             }
         });
     }, { threshold: 0.15 });
